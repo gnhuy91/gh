f78fffdcd18813ce3fc4853d7e8b22418228dfe4
Huy Giang <gnhuy91@gmail.com>
2016-09-27 13:55:44 +0700

    add host param to gh

--- bash/gh.bash
+++ bash/gh.bash
@@ -1,18 +1,43 @@
+#!/bin/bash
+
 GH_BASE_DIR=${GH_BASE_DIR:-$HOME/src}
+
 function gh() {
-  if [[ $# -ne 2 ]]; then
-    echo "USAGE: gh [user] [repo]"
+  if [[ $# -lt 2 || $# -gt 3 ]]; then
+    echo "USAGE: gh [host:-github.com] [user] [repo]"
     return
   fi
 
-  user=$1
-  repo=$2
+  host=$1
+  user=$2
+  repo=$3
 
-  user_path=$GH_BASE_DIR/github.com/$user
+  if [[ $# == 2 ]]; then
+    host="gh"
+    user=$1
+    repo=$2
+  fi
+
+  case "$host" in
+    "gh") host="github.com"
+    ;;
+    "gl") host="gitlab.com"
+    ;;
+    "bb") host="bitbucket.org"
+    ;;
+    "hr") host="git.heroku.com"
+    ;;
+    "df") host="git.dwarvesf.com"
+    ;;
+    *) echo "unknow host: $host"; return
+    ;;
+  esac
+
+  user_path=$GH_BASE_DIR/$host/$user
   local_path=$user_path/$repo
 
   if [[ ! -d $local_path ]]; then
-    git clone git@github.com:$user/$repo.git $local_path
+    git clone git@$host:$user/$repo.git $local_path
   fi
 
   # If git exited uncleanly, clean up the created user directory (if exists)
